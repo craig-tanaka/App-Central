b3511987bd0d39c1aade76f65ace9e4efd3ed23f
Tanxy Rogue <tanxyrogue@gmail.com>
2020-09-04 11:07:30 +0200

    added the basic functionalty to create a page

--- app-central/app-central.php
+++ app-central/app-central.php
@@ -11,3 +11,20 @@ Author: Craig Tanaka Mudzingwa
 Author URI: http://craigtanaka.web.app
 Text Domain: app-central
 */
+
+function create_pages(){
+    
+    // Create register page
+    wp_insert_post( array(
+        'post_title'     => 'Register',
+        'post_name'      => 'Register',
+        'post_content'   => 'This is the plugin register page',
+        'post_status'    => 'publish',
+        'post_author'    => $user_id, // or "1" (super-admin?)
+        'post_type'      => 'page',
+        'menu_order'     => 1,
+        'comment_status' => 'closed',
+        'ping_status'    => 'closed'
+    ));
+}
+register_activation_hook(__FILE__, 'create_pages');
