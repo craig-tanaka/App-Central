8891419403fe3598c70b6a6c2b89ff9fd2995e81
Tanxy Rogue <tanxyrogue@gmail.com>
2020-09-04 10:27:49 +0200

    added basic wordpress files

---
+++ app-central/app-central.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @package AppCentral
+*/
+/*
+Plugin Name: App Central
+Plugin URI: 
+Description: This plugin creates a workflow that allows users to register and sign in to your website, in a way you can easily customize.
+Version: aplha1.0.0
+Author: Craig Tanaka Mudzingwa
+Author URI: http://craigtanaka.web.app
+Text Domain: app-central
+*/
