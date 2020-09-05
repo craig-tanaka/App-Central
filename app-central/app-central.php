2405e46968aba3df8f96fd2e874446374f3d4093
Tanxy Rogue <tanxyrogue@gmail.com>
2020-09-05 07:32:09 +0200

    Linked sign in page

--- app-central/app-central.php
+++ app-central/app-central.php
@@ -16,7 +16,7 @@ function create_pages(){
     // Create register page
     wp_insert_post( array(
         'post_title'     => 'Register',
-        'post_name'      => 'register',
+        'post_name'      => 'ap-register',
         'post_status'    => 'publish',
         'post_author'    => $user_id, // or "1" (super-admin?)
         'post_type'      => 'page',
@@ -25,14 +25,29 @@ function create_pages(){
         'ping_status'    => 'closed',
         'page_template'  => 'template-register.php'
     ));
+    // Create signin page
+    wp_insert_post( array(
+        'post_title'     => 'Sign In',
+        'post_name'      => 'ap-signin',
+        'post_status'    => 'publish',
+        'post_author'    => $user_id, // or "1" (super-admin?)
+        'post_type'      => 'page',
+        'menu_order'     => 1,
+        'comment_status' => 'closed',
+        'ping_status'    => 'closed',
+        'page_template'  => 'template-signin.php'
+    ));
 }
 register_activation_hook(__FILE__, 'create_pages');
 
 
 // Ensures plugin pages are displayed with the plugin templates
 function add_page_template($template){
-    if(is_page( 'register' )){
+    if(is_page( 'ap-register' )){
         $template = plugin_dir_path(__FILE__) . 'templates/template-register.php';
+    }
+    if(is_page( 'ap-signin' )){
+        $template = plugin_dir_path(__FILE__) . 'templates/template-signin.php';
     }
     return $template;
 }
