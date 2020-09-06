891560f62cdf12871dae6d243bf1be1226e7ddb2
Tanxy Rogue <tanxyrogue@gmail.com>
2020-09-06 07:47:06 +0200

    added page deletion on plugin deactivate

--- app-central/app-central.php
+++ app-central/app-central.php
@@ -12,6 +12,11 @@ Author URI: http://craigtanaka.web.app
 Text Domain: app-central
 */
 
+
+// Todo: check for register pages on init
+// Todo: check for errors on page activation and deactivation and upgrades
+// Todo: add subfolders index.php's
+
 function create_pages(){
     // Create register page
     wp_insert_post( array(
@@ -37,6 +42,18 @@ function create_pages(){
         'ping_status'    => 'closed',
         'page_template'  => 'template-signin.php'
     ));
+    // Create create-user page
+    wp_insert_post( array(
+        'post_title'     => 'App Central - Create User',
+        'post_name'      => 'ap-signin',
+        'post_status'    => 'publish',
+        'post_author'    => $user_id, // or "1" (super-admin?)
+        'post_type'      => 'page',
+        'menu_order'     => 1,
+        'comment_status' => 'closed',
+        'ping_status'    => 'closed',
+        'page_template'  => 'template-signin.php'
+    ));
 }
 register_activation_hook(__FILE__, 'create_pages');
 
@@ -52,3 +69,13 @@ function add_page_template($template){
     return $template;
 }
 add_filter( 'template_include', 'add_page_template', 99 );
+
+
+// Move plugin pages to trash on plugin deactivation
+function move_pages_to_trash(){
+    $post = get_page_by_path('ap-register');
+    wp_delete_post($post->ID, true);
+    $post = get_page_by_path('ap-signin');
+    wp_delete_post($post->ID, true);
+}
+register_deactivation_hook( __FILE__, 'move_pages_to_trash' );
